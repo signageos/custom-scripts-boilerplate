6b42c5dd1089ae1b9b9b5451b8305ac2ccbbc7b3
lukdanek <luk.danek@gmail.com>
2025-03-04 16:34:11 +0000

    updated browser script on tizen with ES5 syntax and postResutl to propagate script response back to cloud

--- tizen/setBrightness.js
+++ tizen/setBrightness.js
@@ -1,7 +1,17 @@
-const brightness = sos.config.brightness;
+// legacy devices require ES5-compatible syntax
+// beware or using "modern" javascript if you want your script to be generally applicable
+var brightness = sos.config.brightness;
 
 console.log('Will set brightness to ' + brightness);
 
 sos.management.screen.setBrightness('03:00:00', brightness, '23:00:00', brightness)
-	.then((result) => console.log('ok', result))
-	.catch((e) => console.error('failed', e));
+	.then(function(result) {
+		console.log('ok', result);
+		// pass script response back to cloud
+		postResult("Success, brightness set to " + brightness);
+	})
+	.catch(function(e) {
+		console.error('failed', e);
+		// pass script response back to cloud
+		postResult("Failed to set brightness " + brightness);
+	});
